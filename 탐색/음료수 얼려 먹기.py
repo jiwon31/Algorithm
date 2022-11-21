46d15fd499f777a14ce15d152fedaf96af6cdf9c
jiwon Nam <swm10099@naver.com>
2022-11-22 03:08:50 +0900

    DFS

---
+++ 탐색/음료수 얼려 먹기.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+
+graph = []
+for _ in range(n):
+  graph.append(list(map(int, input())))
+
+def dfs(x, y):
+  # 주어진 범위를 벗어나는 경우에는 즉시 종료
+  if x <= -1 or x >= n or y <= -1 or y >= m:
+    return False
+  # 현재 노드를 아직 방문하지 않았다면
+  if graph[x][y] == 0:
+    # 해당 노드 방문 처리
+    graph[x][y] = 1
+    # 상, 하, 좌, 우 위치도 모두 재귀적으로 호출
+    dfs(x - 1, y)
+    dfs(x + 1, y)
+    dfs(x, y - 1)
+    dfs(x, y + 1)
+    return True
+  return False
+
+
+result = 0
+for i in range(n):
+  for j in range(m):
+    if dfs(i, j) == True:
+      result += 1
+
+print(result)
