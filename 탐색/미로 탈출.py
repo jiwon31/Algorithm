a43695567c491e5d6f795091388d61b63533e86b
jiwon Nam <swm10099@naver.com>
2022-11-23 00:45:37 +0900

    BFS

---
+++ 탐색/미로 탈출.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input())))
+
+# 상 하 좌 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        # 현재 위치에서 네 방향으로의 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            # 괴물이 있는 부분은 무시
+            if graph[nx][ny] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+              graph[nx][ny] = graph[x][y] + 1
+              queue.append((nx, ny))
+    return graph[n - 1][m - 1]
+
+print(bfs(0, 0))
+            
+
