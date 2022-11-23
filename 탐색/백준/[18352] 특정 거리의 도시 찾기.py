87a29255ed27819ace3ce7001d675c5964e6a185
jiwon Nam <swm10099@naver.com>
2022-11-24 03:27:41 +0900

    [백준] BFS

---
+++ 탐색/백준/[18352] 특정 거리의 도시 찾기.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+# 모든 도시에 대한 최단 거리 초기화
+distance = [-1] * (n + 1)
+distance[x] = 0 # 출발 도시까지의 거리는 0
+
+q = deque([x])
+while q:
+    now = q.popleft()
+    for next_node in graph[now]:
+        # 아직 방문하지 않은 도시라면
+        if distance[next_node] == -1:
+            distance[next_node] = distance[now] + 1
+            q.append(next_node)
+
+check = False
+for i in range(1, n + 1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if check == False:
+    print(-1)
