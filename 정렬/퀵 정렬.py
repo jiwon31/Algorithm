09fd239c049a0483eccebab8d63df75e12c2fe1c
jiwon Nam <swm10099@naver.com>
2022-12-05 19:04:44 +0900

    [정렬] 퀵 정렬

---
+++ 정렬/퀵 정렬.py
@@ -0,0 +1,43 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array, start, end):
+  if start >= end: # 원소가 1개인 경우 종료
+    return
+  pivot = start # 피벗은 첫 번째 원소
+  left = start + 1
+  right = end
+  while left <= right:
+    # 피벗보다 큰 데이터를 찾을 때까지 반복
+    while left <= end and array[left] <= array[pivot]:
+      left += 1
+    # 피벗보다 작은 데이터를 찾을 때까지 반복
+    while right > start and array[right] >= array[pivot]:
+      right -= 1
+    if left > right: # 엇갈렸다면 작은 데이터와 피벗을 교체
+      array[right], array[pivot] = array[pivot], array[right]
+    else: # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+      array[left], array[right] = array[right], array[left]
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+    quick_sort(array, 0, right - 1)
+    quick_sort(array, right + 1, end)
+
+quick_sort(array, 0, len(array) - 1)
+print(array)
+
+# -----------------------------------------------------------------
+
+# 위의 방식보다 시간 면에서는 조금 비효율적이지만 더 직관적이다.
+def quick_sort2(array):
+  if len(array) <= 1:
+    return array
+
+  pivot = array[0]
+  tail = array[1:] # 피벗을 제외한 리스트
+
+  left_side = [x for x in tail if x <= pivot] # 분할된 왼쪽 부분
+  right_side = [x for x in tail if x > pivot] # 분할된 오른쪽 부분
+
+  # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬을 수행하고, 전체 리스트를 반환
+  return quick_sort2(left_side) + [pivot] + quick_sort2(right_side)
+
+print(quick_sort2(array))
