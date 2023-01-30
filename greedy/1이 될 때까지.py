5dba63e38404682a7a7ed6561a0d46ecfafbb7a8
jiwon Nam <swm10099@naver.com>
2023-01-31 04:28:35 +0900

    [그리디] 1이 될 때까지

--- greedy/1이 될 때까지.py
+++ greedy/1이 될 때까지.py
@@ -13,3 +13,18 @@ while n > 1:
   result += 1
 
 print(result)
+
+# ---------------------------------------------
+# 내 풀이
+n, k = map(int, input().split())
+count = 0
+
+while n != 1:
+  if n % k == 0:
+    n //= k
+    count += 1
+  else:
+    n -= 1
+    count += 1
+
+print(count)
