f6e26099d59c156ece5db2806112ba8c77515c08
jiwon Nam <swm10099@naver.com>
2023-01-31 23:18:07 +0900

    [그리디] 볼링공 고르기

--- greedy/볼링공 고르기.py
+++ greedy/볼링공 고르기.py
@@ -11,3 +11,20 @@ for i in range(1, m + 1):
   result += array[i] * n
 
 print(result)
+
+# -----------------------------------------------
+# 내 풀이
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+
+array = [0] * (m + 1)
+for i in data:
+  array[i] += 1
+
+result = 0
+for i in range(1, m + 1):
+# A가 고른 공의 개수 * A가 고른 공의 개수 제외한 볼링공의 개수
+  count = array[i] * sum(array[i + 1:]) 
+  result += count
+
+print(result)
