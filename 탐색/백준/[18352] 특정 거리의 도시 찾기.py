5d1f6b909822e372759c3ca2210ff7a89d1cd182
jiwon Nam <swm10099@naver.com>
2023-02-21 23:49:00 +0900

    [백준] 특정 거리의 도시 찾기

--- 탐색/백준/[18352] 특정 거리의 도시 찾기.py
+++ 탐색/백준/[18352] 특정 거리의 도시 찾기.py
@@ -1,4 +1,6 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 n, m, k, x = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
@@ -19,6 +21,43 @@ while q:
         if distance[next_node] == -1:
             distance[next_node] = distance[now] + 1
             q.append(next_node)
+
+check = False
+for i in range(1, n + 1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if check == False:
+    print(-1)
+
+# --------------------------------------------------
+# 다익스트라
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append((b, 1))
+
+q = []
+heapq.heappush(q, (0, x))
+distance[x] = 0
+while q:
+    dist, now = heapq.heappop(q)
+    if distance[now] < dist:
+        continue
+    for i in graph[now]:
+        cost = dist + i[1]
+        if cost < distance[i[0]]:
+            distance[i[0]] = cost
+            heapq.heappush(q, (cost, i[0]))
 
 check = False
 for i in range(1, n + 1):
