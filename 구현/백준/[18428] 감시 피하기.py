b4b36eaf2250281757f4d6dd2d8da622c8629168
jiwon Nam <swm10099@naver.com>
2023-03-20 20:01:48 +0900

    [백준] 감시 피하기

--- 구현/백준/[18428] 감시 피하기.py
+++ 구현/백준/[18428] 감시 피하기.py
@@ -76,8 +76,12 @@ else:
 # DFS
 n = int(input())
 graph = []
+teachers = []
 for i in range(n):
     graph.append(list(input().split()))
+    for j in range(n):
+        if graph[i][j] == 'T':
+            teachers.append((i, j))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -97,11 +101,9 @@ check = False
 def dfs(count):
     global check
     if count == 3:
-        for i in range(n):
-            for j in range(n):
-                if graph[i][j] == 'T':
-                    if watch(i, j):
-                        return check
+        for i, j in teachers:
+            if watch(i, j):
+                return
         check = True
         return
 
@@ -116,6 +118,56 @@ def dfs(count):
 
 dfs(0)
 if check:
+    print('YES')
+else:
+    print('NO')
+
+# -----------------------------------------------------
+# combinations
+from itertools import combinations
+
+n = int(input())
+graph = []
+empty_space = []
+teachers = []
+for i in range(n):
+    graph.append(list(input().split()))
+    for j in range(n):
+        if graph[i][j] == 'X':
+            empty_space.append((i, j))
+        elif graph[i][j] == 'T':
+            teachers.append((i, j))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def check_student(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        while 0 <= nx < n and 0 <= ny < n and graph[nx][ny] != "O":
+            if graph[nx][ny] == "S":
+                return True
+            nx += dx[i]
+            ny += dy[i]
+    return False
+
+result = False
+obstacles = list(combinations(empty_space, 3))
+for obstacle in obstacles:
+    count = 0
+    for x, y in obstacle:
+        graph[x][y] = 'O'
+    for i, j in teachers:
+        if not check_student(i, j):
+            count += 1
+    if count == len(teachers):
+        result = True
+        break
+    for x, y in obstacle:
+        graph[x][y] = 'X'
+
+if result:
     print('YES')
 else:
     print('NO')
